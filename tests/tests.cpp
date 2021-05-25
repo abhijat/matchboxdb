1243d6e0363eece0b38e31c32ae21caf0d48ffaf
Abhijat Malviya <abhijat@abhijat.io>
2021-05-25 15:08:36 +0530

    adds tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,17 @@
 #include "gtest/gtest.h"
+#include "../src/page_cache.h"
 
-TEST(ASimpleTest, DumbEqCheck) {
-    EXPECT_EQ(1, 1);
+TEST(PageCacheTests, FileNameFromTableName) {
+    std::string table_name{"foobar"};
+    auto file_name = page_cache::file_name_from_table_name(table_name);
+    ASSERT_EQ("foobar.mbx", file_name);
+}
+
+TEST(PageCacheTests, CacheKeyGen) {
+    std::string table_name{"foobar"};
+    uint32_t page_id{100};
+    auto page_type = page::PageType::Metadata;
+    auto key = page_cache::generate_cache_key(page_id, table_name, page_type);
+    ASSERT_EQ("md::foobar::100", key);
 }
 
