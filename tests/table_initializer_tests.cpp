cf5e6840f8f7bf6db77e4c0f93002904fac87e4b
Abhijat Malviya <abhijat@abhijat.io>
2021-05-29 13:50:12 +0530

    tests for new pages

--- tests/table_initializer_tests.cpp
+++ tests/table_initializer_tests.cpp
@@ -7,6 +7,7 @@
 #include "../src/streamutils.h"
 #include "../src/metadata_page.h"
 #include "../src/slotted_data_page.h"
+#include "../src/row_mapping_page.h"
 
 
 class TableInitializationTests : public ::testing::Test {
@@ -40,6 +41,7 @@ TEST_F(TableInitializationTests, FileIsCreated) {
 TEST_F(TableInitializationTests, PageCalculations) {
     std::ifstream i{path_to_file, std::ios::binary};
     auto metadata_page = stream_utils::read_nth_metadata_page(i);
+    ASSERT_EQ(metadata_page.page_type(), page::PageType::Metadata);
     ASSERT_EQ(metadata_page.n_data_pages(), 2048);
     ASSERT_EQ(metadata_page.n_rowmap_pages(), 128);
 }
@@ -48,4 +50,10 @@ TEST_F(TableInitializationTests, ReadRandomDataPages) {
     std::ifstream i{path_to_file, std::ios::binary};
     auto data_page = stream_utils::read_nth_data_page(i, 100);
     ASSERT_EQ(data_page.page_type(), page::PageType::Data);
+}
+
+TEST_F(TableInitializationTests, ReadRandomRowmapPages) {
+    std::ifstream i{path_to_file, std::ios::binary};
+    auto row_map_page = stream_utils::read_nth_row_mapping_page(i, 100);
+    ASSERT_EQ(row_map_page.page_type(), page::PageType::RowMap);
 }
